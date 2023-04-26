85f1ef998019a5c531b66b33f435d5184ddf6fe4
ferrary7 <ary7sharma@gmail.com>
2023-04-26 10:22:30 +0530

    React-app initialized and Components created

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import IDE from "./Components/Ide";
+
+function App() {
+  return (
+    <div className="App">
+      <IDE />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Ide.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Ide.css";
+
+const IDE = () => {
+  return <div className="ide-main">IDE</div>;
+};
+
+export default IDE;
